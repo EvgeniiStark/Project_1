5837388fa608a5cdb0a9814291bd243f1c91da18
EvgeniiStark <124917341+EvgeniiStark@users.noreply.github.com>
2023-02-13 21:40:34 +0500

    Add files via upload

---
+++ precache-manifest.be61e99a869e707f4b983f26c117d205.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "0581978c661b7d51966c",
+    "url": "/Project_1/static/css/main.59c55e81.chunk.css"
+  },
+  {
+    "revision": "0581978c661b7d51966c",
+    "url": "/Project_1/static/js/main.0581978c.chunk.js"
+  },
+  {
+    "revision": "826062b24bfdc73ce3e6",
+    "url": "/Project_1/static/js/1.826062b2.chunk.js"
+  },
+  {
+    "revision": "35945f1499fa097f3c07",
+    "url": "/Project_1/static/css/2.efb37e62.chunk.css"
+  },
+  {
+    "revision": "35945f1499fa097f3c07",
+    "url": "/Project_1/static/js/2.35945f14.chunk.js"
+  },
+  {
+    "revision": "338d231afd7115ff9aa3",
+    "url": "/Project_1/static/css/3.c4b2dd36.chunk.css"
+  },
+  {
+    "revision": "338d231afd7115ff9aa3",
+    "url": "/Project_1/static/js/3.338d231a.chunk.js"
+  },
+  {
+    "revision": "cee2d46f70aa025e19b3",
+    "url": "/Project_1/static/js/4.cee2d46f.chunk.js"
+  },
+  {
+    "revision": "1559b81554d959ee81d2",
+    "url": "/Project_1/static/js/runtime~main.1559b815.js"
+  },
+  {
+    "revision": "4e3b36cdfb50531afcd8142eaed0a45e",
+    "url": "/Project_1/static/media/avatar.4e3b36cd.jpg"
+  },
+  {
+    "revision": "6921c9f6ff89071a37a99cecd0bc0ed3",
+    "url": "/Project_1/static/media/Time.6921c9f6.gif"
+  },
+  {
+    "revision": "f6c5c75691af7352d3a50a90a618b9b4",
+    "url": "/Project_1/static/media/make-a-unique-logo-for-your-company.f6c5c756.png"
+  },
+  {
+    "revision": "57a0bbff9a09554212089acc322e0639",
+    "url": "/Project_1/static/media/cat.57a0bbff.jpg"
+  },
+  {
+    "revision": "6ec5d793c3904a400436a80faf73b683",
+    "url": "/Project_1/index.html"
+  }
+];
